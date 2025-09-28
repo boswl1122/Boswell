a001c0775fa150cda87618bde5907cff1cc695f0
Ishaan Pomichter <team@myboswell.com>
2025-09-27 17:49:40 -0700

    Add viewport meta for mobile scaling in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,15 @@
+// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Enables proper mobile scaling */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Optional: nicer mobile address-bar color */}
+        {/* <meta name="theme-color" content="#b45a06" /> */}
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
